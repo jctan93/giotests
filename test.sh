c9daf3457ec9316a00c0d730fa0f8f44e39b1ce7
Tan Jian Chern <jctan93@jctan93nuc>
2017-09-28 13:59:57 +0800

    added shell script and yaml files

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+#echo 'Acquire::http::Proxy "http://proxy-png.intel.com:911";' > /etc/apt/apt.conf
+#if [ $? -eq 0 ]; then
+#	lava-test-case acquire-proxy --result pass
+#else
+#	lava-test-case acquire-proxy --result fail
+#fi
+#gcc -s -Wall -Wstrict-prototypes rtc/rtctest.c -o rtctest
+#gcc -s -Wall -Wstrict-prototypes rtc/rtc_cmos_test.c -o rtc_cmos_test
+
+#./rtctest
+#./rtc_cmos_test
+
+#python gui_startup.py
+python siv_test_collateral/siv_val-io-test-scripts/system/rtc_gio.py -i $sut_ip -t check_driver
